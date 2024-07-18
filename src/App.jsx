13c227022cff9f7d8d88bb980a24ba9298122755
Oh-seo-jin <seojin2414@gmail.com>
2024-07-18 11:46:42 +0900

    생성 : 상품카드 component 생성

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import ProductCard from './components/ProductCard';
+import './App.css';
+
+function App() {
+  let n = 2;
+  return (
+    <div className='w-screen h-screen'>
+      {/* header */}
+      <div className='bg-black w-screen h-16'>
+        <div className=''>장바구니</div>
+      </div>
+      {/* main */}
+      <div className='p-6'>
+        <h1 className='font-bold text-2xl mt-4 mb-1'>신발 상품 목록</h1>
+        <p className='text-base mb-6'>현재 {n}개의 상품이 있습니다.</p>
+        <div className='grid grid-cols-2 gap-4'>
+          <ProductCard path="/product1.png" name="브랜드A" info="편안하고 착용감이 좋은 신발" price="35,000원"/>
+          <ProductCard path="/product2.png" name="브랜드A" info="힙한 컬러가 매력적인 신발" price="25,000원"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,12 @@
 export default function ProductCard({path, name, info, price}) {
     return (
-        <div className="underline">
-            <img src={path}></img>
-            <div>
-                <h3>{name}</h3>
-                <p>{info}</p>
-                <p>{price}</p>
+        <div className="w-full h-[280px] flex flex-col items-center justify-center rounded-2xl border border-gray-100 overflow-hidden">
+            <img src={path} className="w-full h-1/2 object-cover"/>
+            <div className="w-full h-1/2 px-4 py-4 flex flex-col gap-1">
+                <h3 className="font-bold text-base">{name}</h3>
+                <p className="text-sm text-gray-500">{info}</p>
+                <p className="font-bold text-base">{price}</p>
+                <button className="bg-black text-white text-xs p-1 w-11 rounded-xl">담기</button>
             </div>
         </div>
     )
