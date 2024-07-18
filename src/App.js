13971753c1ff20806b28535957608fbf16344f63
Oh-seo-jin <seojin2414@gmail.com>
2024-07-18 09:41:44 +0900

    생성 : React 프로젝트 생성 및 Tailwind 설치

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import ProductCard from './components/ProductCard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductCard/>
+    </div>
+  );
+}
+
+export default App;
