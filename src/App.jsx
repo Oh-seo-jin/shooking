8c41ee5659b9c270f6dffce512196d06d7473225
Oh-seo-jin <seojin2414@gmail.com>
2024-07-18 12:10:15 +0900

    기능 : 장바구니 실시간 개수 업데이트 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import ProductCard from './components/ProductCard';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
   let n = 2;
+  const [count, setCount] = useState(0);
+
   return (
     <div className='w-screen h-screen'>
       {/* header */}
-      <div className='bg-black w-screen h-16'>
-        <div className=''>장바구니</div>
+      <div className='bg-black w-screen h-16 flex flex-row justify-end items-center px-6'>
+        <p className='text-white mr-1'>장바구니</p>
+        <p className='bg-white text-black font-bold rounded-full w-6 h-6 text-center'>{count}</p>
       </div>
       {/* main */}
       <div className='p-6'>
         <h1 className='font-bold text-2xl mt-4 mb-1'>신발 상품 목록</h1>
         <p className='text-base mb-6'>현재 {n}개의 상품이 있습니다.</p>
         <div className='grid grid-cols-2 gap-4'>
-          <ProductCard path="/product1.png" name="브랜드A" info="편안하고 착용감이 좋은 신발" price="35,000원"/>
-          <ProductCard path="/product2.png" name="브랜드A" info="힙한 컬러가 매력적인 신발" price="25,000원"/>
+          <ProductCard path="/product1.png" name="브랜드A" info="편안하고 착용감이 좋은 신발" price="35,000원" count={count} setCount={setCount}/>
+          <ProductCard path="/product2.png" name="브랜드A" info="힙한 컬러가 매력적인 신발" price="25,000원" count={count} setCount={setCount}/>
         </div>
       </div>
     </div>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,12 @@
-export default function ProductCard({path, name, info, price}) {
+import React, { createContext, useState} from 'react';
+
+export default function ProductCard({path, name, info, price, count, setCount}) {
+    const [disabled, setDisabled] = useState(false);
+    const handleClick = () => {
+        setDisabled(true);
+        setCount(count + 1);
+    }
+    
     return (
         <div className="w-full h-[280px] flex flex-col items-center justify-center rounded-2xl border border-gray-100 overflow-hidden">
             <img src={path} className="w-full h-1/2 object-cover"/>
@@ -6,7 +14,7 @@ export default function ProductCard({path, name, info, price}) {
                 <h3 className="font-bold text-base">{name}</h3>
                 <p className="text-sm text-gray-500">{info}</p>
                 <p className="font-bold text-base">{price}</p>
-                <button className="bg-black text-white text-xs p-1 w-11 rounded-xl">담기</button>
+                <button onClick={handleClick} className={`bg-black text-white text-xs p-1 w-11 rounded-xl ${disabled ? 'bg-gray-300 text-black font-bold': ''}`}>{disabled ? "담김!" : "담기"}</button>
             </div>
         </div>
     )
